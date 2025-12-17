afc8b114cc3891eef83f5e01471302dde4f2227c
chlghksgml01 <chlghksgml01@naver.com>
2025-12-17 22:41:27 +0900

    [feat] 서버 접속

---
+++ Assets/1.Scripts/Backend/BackendManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using BackEnd;
+
+public class BackendManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        BackendSetup();
+    }
+
+    private void BackendSetup()
+    {
+        // 뒤끝 초기화
+        BackendReturnObject bro = Backend.Initialize();
+
+        // 뒤끝 초기화에 대한 응답값
+        if (bro.IsSuccess())
+        {
+            // 초기화 성공시 statusCode 204 Succees
+            Debug.Log($"초기화 성공 : {bro}");
+        }
+        else
+        {
+            // 초기화 실패시 statusCode 400대 에러 발생
+            Debug.Log($"초기화 실패 : {bro}");
+        }
+    }
+}
