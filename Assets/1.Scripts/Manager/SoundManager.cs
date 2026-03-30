fb4ea5f000d17ac28a736c3d54c4746d618dcb73
chlghksgml01 <chlghksgml01@naver.com>
2026-03-30 16:50:52 +0900

    SoundManager 추가

---
+++ Assets/1.Scripts/Manager/SoundManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public enum SFXType
+{
+    GameStart,
+    GameOver,
+    SelectBlock,
+    PlaceBlock,
+    ClearLine,
+    ClickUI
+}
+
+public class SoundManager : Singleton<SoundManager>
+{
+    private AudioSource _audioSource;
+    [SerializeField] private AudioClip _gameStart;
+    [SerializeField] private AudioClip _gameOver;
+    [SerializeField] private AudioClip _selectBlock;
+    [SerializeField] private AudioClip _placeBlock;
+    [SerializeField] private AudioClip[] _clearLine;
+    [SerializeField] private AudioClip _clickUI;
+
+
+    protected override void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySFX(SFXType type, int comboCount = 0)
+    {
+        switch (type)
+        {
+            case SFXType.GameStart:
+                _audioSource.PlayOneShot(_gameStart);
+                break;
+            case SFXType.GameOver:
+                _audioSource.PlayOneShot(_gameOver);
+                break;
+            case SFXType.SelectBlock:
+                _audioSource.PlayOneShot(_selectBlock);
+                break;
+            case SFXType.PlaceBlock:
+                _audioSource.PlayOneShot(_placeBlock);
+                break;
+            case SFXType.ClearLine:
+                PlayComboSFX(comboCount);
+                break;
+            case SFXType.ClickUI:
+                _audioSource.PlayOneShot(_clickUI);
+                break;
+
+        }
+    }
+
+    private void PlayComboSFX(int comboCount)
+    {
+        if (comboCount <= 0)
+            return;
+        int index = Mathf.Min(comboCount - 1, _clearLine.Length - 1);
+        _audioSource.PlayOneShot(_clearLine[index]);
+    }
+}
