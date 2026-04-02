3c245386d90dc4ac601dc5b3fffc1a4d4649b473
chlghksgml01 <chlghksgml01@naver.com>
2026-04-02 23:53:49 +0900

    인게임 뒤로가기버튼으로 설정 키기

---
+++ Assets/1.Scripts/UI/SettingUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SettingUI : MonoBehaviour
+{
+    [SerializeField] private SettingPanel _settingPanel;
+    [SerializeField] private GameOverUI _GameOverUI;
+
+    private void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame && !_settingPanel.isActiveAndEnabled && !_GameOverUI.isActiveAndEnabled)
+        {
+            _settingPanel.Open();
+            SoundManager.Instance.PlaySFX(SFXType.ClickUI);
+        }
+    }
+}
