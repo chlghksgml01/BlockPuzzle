023887a24cbe0883bcde96d9dcb6001791949670
chlghksgml01 <chlghksgml01@naver.com>
2026-07-20 16:04:34 +0900

    feat: BP-4 노드에 레벨 추가

--- Assets/1.Scripts/LevelMap/LevelNodeView.cs
+++ Assets/1.Scripts/LevelMap/LevelNodeView.cs
@@ -1,4 +1,6 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// 가상화 풀에서 재사용되는 레벨 노드 하나의 뷰.
@@ -6,6 +8,10 @@ using UnityEngine;
 [RequireComponent(typeof(RectTransform))]
 public sealed class LevelNodeView : MonoBehaviour
 {
+    [Header("UI")]
+    [Tooltip("노드에 표시할 레벨 번호 텍스트")]
+    [SerializeField] private TMP_Text _levelText;
+
     public RectTransform RectTransform { get; private set; }
     public int NodeIndex { get; private set; } = -1;
 
@@ -19,5 +25,8 @@ public sealed class LevelNodeView : MonoBehaviour
         NodeIndex = nodeIndex;
         RectTransform.anchoredPosition = anchoredPosition;
         gameObject.name = $"LevelNode_{nodeIndex + 1}";
+
+        if (_levelText != null)
+            _levelText.text = (nodeIndex + 1).ToString();
     }
 }
